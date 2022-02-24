69fc2fec847c54e4de44f1b1a8f008d11196be86
mukhid <mukhid976@gmail.com>
2022-02-24 19:54:41 +0700

    TA-React-Lanjutan

---
+++ soal-2/src/App.js
@@ -0,0 +1,76 @@
+import { useEffect, useState } from "react";
+import CardProduct from "./component/CardProduct";
+import CartListItem from "./component/CardListItem";
+import Navbar from "./component/Navbar";
+
+import menus from "./data-dummy"
+
+function App() {
+  const [total, setTotal] = useState(0);
+  const [purchasedItem, setPurchasedItem] = useState(0);
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (id) => {
+    if(id == 0){
+      
+    }
+  };
+
+  const decreaseCartAmount = (id) => {};
+
+  const increaseCartAmount = (id) => {};
+
+  return (
+    <div className="bg-secondary">
+      <Navbar totalItem={purchasedItem} />
+      <div className="container py-5">
+        <div className="row">
+          <div className="col-md-8">
+            <div className="card w-100">
+              <div className="card-body">
+                <div className="row">
+                  {menus.map((menu) => (
+                    <div
+                      key={menu.id}
+                      className="col-md-4 col-sm-6 col-12 my-2"
+                    >
+                      <CardProduct
+                        {...menu}
+                        addToCart={() => addToCart(menu.id)}
+                      />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <ol className="list-group">
+              {cart.map((c) => {
+                return (
+                  <CartListItem
+                    key={c.id}
+                    {...c}
+                    increase={() => increaseCartAmount(c.id)}
+                    decrease={() => decreaseCartAmount(c.id)}
+                  />
+                );
+              })}
+              <li className="list-group-item d-flex justify-content-between align-items-start">
+                <div className="ms-2 me-auto">
+                  <div className="fw-bold">Total Harga</div>
+                </div>
+                {new Intl.NumberFormat("en-US", {
+                  style: "currency",
+                  currency: "USD"
+                }).format(total)}
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
